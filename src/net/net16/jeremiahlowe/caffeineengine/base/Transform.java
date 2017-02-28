4cc7d5de30f156701040e5b5f227ca64ada333e7
abc123me <jeremiah_abc123me@hotmail.com>
2017-02-27 19:24:12 -0500

    Finished Transform and cloning

--- src/net/net16/jeremiahlowe/caffeineengine/base/Transform.java
+++ src/net/net16/jeremiahlowe/caffeineengine/base/Transform.java
@@ -3,7 +3,14 @@ package net.net16.jeremiahlowe.caffeineengine.base;
 import net.net16.jeremiahlowe.bettercollections.Rotation;
 import net.net16.jeremiahlowe.bettercollections.vector.Vector2;
 
-public class Transform {
+public class Transform implements Cloneable{
 	public Vector2 position;
 	public Rotation rotation;
+	@Override
+	public Transform clone(){
+		Transform out = new Transform();
+		out.position = position.clone();
+		out.rotation = rotation.clone();
+		return out;
+	}
 }
