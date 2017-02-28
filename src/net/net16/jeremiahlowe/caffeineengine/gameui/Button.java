968fcf24cb553c41495949ee986670d07e19c79e
abc123me <jeremiah_abc123me@hotmail.com>
2017-02-28 18:23:35 -0500

    ROUND EDGES!!!!!!!!!!!!!!!!!!

--- src/net/net16/jeremiahlowe/caffeineengine/gameui/Button.java
+++ src/net/net16/jeremiahlowe/caffeineengine/gameui/Button.java
@@ -12,6 +12,7 @@ public class Button extends UIElement implements Runnable{
 	public String text = "New Button";
 	public Vector2 borderSize = new Vector2(5, 15);
 	public int buttonToTriggerUpon = 1;
+	public int buttonBorderRoundness = 5;
 	private Bounds bounds = new Bounds();
 	
 	public Button(Input input, Runnable onClick, String text){
@@ -31,7 +32,7 @@ public class Button extends UIElement implements Runnable{
 		bounds.h = txtH + borderSize.y;
 		g.fillRect((int) bounds.x, (int) bounds.y, (int) bounds.w, (int) bounds.h);
 		g.setColor(Color.BLACK);
-		g.drawRect((int) bounds.x, (int) bounds.y, (int) bounds.w, (int) bounds.h);
+		g.drawRoundRect((int) bounds.x, (int) bounds.y, (int) bounds.w, (int) bounds.h, buttonBorderRoundness, buttonBorderRoundness);
 		Vector2 strSize = new Vector2(0, 0);
 		strSize.x = position.x + borderSize.x / 2;
 		strSize.y = position.y + borderSize.y / 2 + 3 * (txtH / 4);
